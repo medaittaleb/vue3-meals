892bb17d0aa43a664857df50cc9675c873b7c99f
Mohamed Ait Taleb <yamixlive@gmail.com>
2023-02-26 21:36:54 +0100

    install and init store routes

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col p-8 justify-center">
+    <input type="text" 
+        placeholder="Search for Meals" 
+        class="rounded border-2 border-gray-200 w-full"
+    >    
+    <div class="flex gap-3 mt-3 justify-center ">
+      <router-link :to="{ name: 'byLetter', params: { leftter } }" v-for="leftter of letters" :key="leftter">{{ leftter }}</router-link>
+    </div>
+</div>
+
+</template>
+
+<script setup>
+import { computed, onMounted } from 'vue';
+import axiosClient from '../axiosClient.js'
+import store from '../store';
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+
+onMounted(async () => {
+  const response = await axiosClient.get('/list.php?i=list')
+  console.log(response.data);
+})
+
+</script>
